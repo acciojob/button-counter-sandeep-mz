0a89ea98e6dced9e4827bc15201208cb9f1409e2
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-12-12 13:51:01 +0000

    1st update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  // Destructuring useState to get count and setCount
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button Clicked {count} times</p>
+      {/* Correcting onClick function */}
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
